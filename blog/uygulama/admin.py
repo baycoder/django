99a1e21964645e4ef3deddb5c4b6829784bdd9e7
baycoder <integercode@gmail.com>
2017-07-01 14:05:19 +0300

    DjangoGirls

---
+++ blog/uygulama/admin.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.contrib import admin
+from .models import Post
+
+
+class PostAdmin(mo)
+
+
+
+admin.site.register(Post)
+
+
+
+
+
